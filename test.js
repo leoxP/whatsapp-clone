c291b9749b684f34a2b9509db14f2f56ad9eb486
leoxP <patrabaibhab@gmail.com>
2024-01-07 13:26:08 +0530

    initialize

---
+++ test.js
@@ -0,0 +1,11 @@
+//Tech used
+
+// React
+// Firebase Firstore Realtime db
+// Material UI
+// React Router
+// React Context API
+// ^REDUX
+// Google Authentication
+// Deploy using Firebase
+
---
+++ whats-app-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBEVDQyXg8sP7VM1Nb904Lo8gjxO8N8b1M",
+    authDomain: "whatsapp-clone-59979.firebaseapp.com",
+    projectId: "whatsapp-clone-59979",
+    storageBucket: "whatsapp-clone-59979.appspot.com",
+    messagingSenderId: "66698926010",
+    appId: "1:66698926010:web:8580cfffdd5cfa19fc6f83",
+    measurementId: "G-ZZPNWY00N1"
+  };
